2bbbbe2cdcc21360a35df8447700bbddcb586f03
Diana Georgieva <40742204+DianaGeorgievaa@users.noreply.github.com>
2019-03-15 22:09:35 +0200

    Update and rename Homeworks/HomeWork2/Task 1/ElectricDevice.h to Homeworks/Homework_2/Task 1/ElectricDevice.h

--- Homeworks/Homework_2/Task 1/ElectricDevice.h
+++ Homeworks/Homework_2/Task 1/ElectricDevice.h
@@ -5,14 +5,14 @@
 class ElectricDevice
 {
 public:
-	ElectricDevice();
-	ElectricDevice(const char* name, int power);
+    ElectricDevice();
+    ElectricDevice(const char* name, int power);
     ElectricDevice(const ElectricDevice & other);
     ElectricDevice &operator=(const ElectricDevice & other);
-	void SetName(const char*name);
-	void SetPower(int);
-	const char*GetName()const;
-	int GetPower()const;
+    void SetName(const char*name);
+    void SetPower(int);
+    const char*GetName()const;
+    int GetPower()const;
     friend std::ostream &operator<<(std::ostream &os, const ElectricDevice & e);
    ~ElectricDevice();
 private:
